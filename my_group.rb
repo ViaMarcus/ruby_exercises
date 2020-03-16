6df2ec301e957c199af37d9b9c746c966bad6771
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-03-16 10:48:22 +0100

    Created my_group exercise file

---
+++ my_group.rb
@@ -0,0 +1,7 @@
+my_group = Array.new
+person_1 = {name:"Leah",age:1,gender:"female"}
+person_2 = {name:"Simba",age:13,gender:"male"}
+person_3 = {name:"Mufasa",age:53,gender:"male"}
+Array.push person_1
+Array.push person_2
+Array.push person_3
